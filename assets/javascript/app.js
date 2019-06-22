1b11c2986dd8ad42dbd0bf7d3e020c2eedd86a66
Flying Serpent <nickprather11@gmail.com>
2019-06-22 15:20:36 -0400

    switched available players' info to be stored in firebase connections instead of root, presence list working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,3 @@
-// Display divs for game title, boxes for players 1 and 2 and status, chatbox, and list of players to choose from.
 var firebaseConfig = {
   apiKey: "AIzaSyCQwhMpGPFS8wqCkRypCXUaVxOe2mZgml0",
   authDomain: "test-7c38b.firebaseapp.com",
@@ -13,8 +12,10 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 var connectionsRef = database.ref("/connections");
+var con;
 var connectedRef = database.ref(".info/connected");
-var player1 = prompt("Enter your name or username: ");
+var name = prompt("Enter your name or username: ");
+var player1 = name;
 var player2 = "Player 2";
 var choices = ["Rock", "Paper", "Scissors"];
 function displayChoices() {
@@ -31,24 +32,51 @@ connectedRef.on("value", function(snap) {
   // If they are connected..
   if (snap.val()) {
     // Add user to the connections list.
-    var con = connectionsRef.push(true);
-
+    // log to root database
     // Remove user from the connection list when they disconnect.
-    con.onDisconnect().remove();
   }
 });
 // Number of online users is the number of objects in the presence list.
 connectionsRef.on("value", function(snapshot) {
   $("#connected-viewers").text(snapshot.numChildren() + " players available");
-  // Display "names" of connected players
 });
 
-var player1 = prompt("Enter your name or username: ");
 //initial display
 $("#player1").html("<h3>" + player1 + "</h3>");
 $("#status").html("<h3>" + "Vs." + "</h3>");
 $("#player2").html("<h3>" + player2 + "</h3>");
-//Show who's online and list of users in available player div
+
+if (player2 !== "Player 2") {
+  displayChoices();
+}
+//Show who's online and list of users in available player div.
+//When someone loads the page and enters their name, push it to an array
+connectionsRef.once("value").then(function(snapshot) {
+  console.log(snapshot.val());
+  con = connectionsRef.push({
+    name: name,
+    timeAdded: firebase.database.ServerValue.TIMESTAMP
+  });
+  con.onDisconnect().remove();
+});
+connectionsRef.on(
+  "child_added",
+  function(snapshot) {
+    var availPlayer = $("<div>").text(snapshot.val().name);
+    availPlayer.attr("id", snapshot.key);
+    $("#availPlayers").append(availPlayer);
+    // Handle the errors
+  },
+  function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  }
+);
+
+connectionsRef.on("child_removed", function(snapshot) {
+  console.log(snapshot);
+  $("#" + snapshot.key).remove();
+});
+
 //onClick to chose opponent from this list
-//onClick to chose rock, paper, or scissors
+//onClick to chose rock, paper, or scissors only if an opponent is chosen.
 //Logic for RPS game
